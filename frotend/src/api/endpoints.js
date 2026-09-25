96e5c82561866603318f2d3c0f931a8a2f7fa57e
Nitin <beldarnitin845@gmail.com>
2026-09-25 21:48:39 +0530

    the deployment error are fixing

--- frotend/src/api/endpoints.js
+++ frotend/src/api/endpoints.js
@@ -1,5 +1,5 @@
 // Development
-export const BASE_URL = "https://citizendesk-pdkz.onrender.com";
+export const BASE_URL = "https://citizendesk-pdkz.onrender.com/api/v1";
 
 // CHANGED: Export them as individual constants instead of one object
 export const SEND_OTP = `${BASE_URL}/auth/send-otp`;
